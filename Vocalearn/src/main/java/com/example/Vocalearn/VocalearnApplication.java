229ce153e8df51beaa424597e5656ebdf10725cb
JackobOnielHasianSidabutar <jackoboniel1@gmail.com>
2025-03-05 14:11:02 +0700

    initializing project

---
+++ Vocalearn/src/main/java/com/example/Vocalearn/VocalearnApplication.java
@@ -0,0 +1,13 @@
+package com.example.Vocalearn;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VocalearnApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(VocalearnApplication.class, args);
+	}
+
+}
---
+++ Vocalearn/src/test/java/com/example/Vocalearn/VocalearnApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.Vocalearn;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VocalearnApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
